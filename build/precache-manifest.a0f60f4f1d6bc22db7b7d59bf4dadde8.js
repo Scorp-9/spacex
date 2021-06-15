a58b7548d625e38bcf62b5fd71f66589a8331295
Prasad <prasadj780@gmail.com>
2021-06-16 02:43:19 +0530

    Initial commit

---
+++ build/precache-manifest.a0f60f4f1d6bc22db7b7d59bf4dadde8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7956e2ae2dd8c57a43d69896912c54ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8fa0fc6419624aee851",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a7da4849d8be4db1e534",
+    "url": "/static/css/main.956d9460.chunk.css"
+  },
+  {
+    "revision": "a8fa0fc6419624aee851",
+    "url": "/static/js/2.6cdbf0dc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.6cdbf0dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7da4849d8be4db1e534",
+    "url": "/static/js/main.58b02fd0.chunk.js"
+  },
+  {
+    "revision": "5268b1c7be79030bb5b5",
+    "url": "/static/js/runtime-main.142b1330.js"
+  },
+  {
+    "revision": "c537d7d180515a2d14e723821dcf5097",
+    "url": "/static/media/loadRocket.c537d7d1.gif"
+  }
+]);
